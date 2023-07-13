26c3e11760f98bcd3014cd92951cbea7173274b2
lepplaju <lepplaju@student.jyu.fi>
2023-07-13 17:06:24 +0300

    Turn based battle + animations

--- app/src/main/java/com/example/lutemon/activities/BattlePage.java
+++ app/src/main/java/com/example/lutemon/activities/BattlePage.java
@@ -2,10 +2,11 @@ package com.example.lutemon.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,12 +25,13 @@ import java.util.Random;
 public class BattlePage extends AppCompatActivity {
 
     private boolean buttonsAreVisible = true;
-    private boolean enemyIsAlive = true;
+    private boolean enemyLutemonIsAlive = true;
+    private boolean userLutemonIsAlive = true;
     private Enemy enemy;
-    Lutemon enemyLutemon;
-    Lutemon userLutemon;
+    private Lutemon enemyLutemon;
+    private Lutemon userLutemon;
 
-    ArrayList<Move> userMoves;
+    private ArrayList<Move> userMoves;
 
     private TextView userLutemonHp;
     private TextView userLutemonName;
@@ -110,37 +112,12 @@ public class BattlePage extends AppCompatActivity {
             } else if (viewId == R.id.move4Btn) {
                 userLutemon.attack(enemyLutemon, 3);
             }
+            userAnimation();
             disableButtons();
-            enemyLutemonHp.setText(enemyLutemon.getHealth() + "/" + enemyLutemon.getMaxHealth());
-
-            if (enemyLutemon.getHealth() <= 0) {
-                enemyIsAlive = false;
-                destroyEnemyLutemon();
-            }
-            if (enemyIsAlive) {
-                Random random = new Random();
-                int attackId = random.nextInt(4);
-                enemyLutemon.attack(userLutemon, attackId);
-                userLutemonHp.setText(userLutemon.getHealth() + "/" + userLutemon.getMaxHealth());
-            }
-            if(userLutemon.getHealth() <= 0){
-                destroyUserLutemon();
-            }
 
         }
     };
 
-    public void destroyEnemyLutemon() {
-        enemyImage.setImageResource(0);
-        enemyLutemonName.setText("");
-        enemyLutemonHp.setText("");
-    }
-
-    public void destroyUserLutemon() {
-        userImage.setImageResource(0);
-        userLutemonName.setText("");
-        userLutemonHp.setText("");
-    }
 
     public void disableButtons() {
         buttonsAreVisible = false;
@@ -158,7 +135,147 @@ public class BattlePage extends AppCompatActivity {
                 move3Btn.setVisibility(View.VISIBLE);
                 move4Btn.setVisibility(View.VISIBLE);
             }
-        }, 5000);
+        }, 2500);
 
+    }
+
+    public void userAnimation() {
+        int[] userLocation = new int[2];
+        int[] enemyLocation = new int[2];
+
+        userImage.getLocationInWindow(userLocation);
+        enemyImage.getLocationInWindow(enemyLocation);
+
+        float startX = userLocation[0];
+        float startY = userLocation[1];
+        float endX = enemyLocation[0];
+        float endY = enemyLocation[1];
+
+        Animation attackAnimation = new TranslateAnimation(0, endX - startX, 0, endY - startY);
+        attackAnimation.setDuration(200);
+        attackAnimation.setFillAfter(true);
+        attackAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (enemyLutemonIsAlive) {
+                            damageVisuals();
+                        }
+
+                    }
+                }, 200);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation moveBackAnimation = new TranslateAnimation(endX - startX, 0, endY - startY, 0);
+                moveBackAnimation.setDuration(200);
+                moveBackAnimation.setFillAfter(true);
+                userImage.startAnimation(moveBackAnimation);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (enemyLutemonIsAlive) {
+                            enemyAnimation();
+                        }
+                    }
+
+                }, 1000);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        userImage.startAnimation(attackAnimation);
+    }
+
+    private void damageVisuals() {
+        enemyLutemonHp.setText(enemyLutemon.getHealth() + "/" + enemyLutemon.getMaxHealth());
+        if (enemyLutemon.getHealth() <= 0) {
+            enemyLutemonIsAlive = false;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    destroyEnemyLutemon();
+                }
+            }, 600);
+        }
+    }
+
+    private void enemyAnimation() {
+        int[] userLocation = new int[2];
+        int[] enemyLocation = new int[2];
+
+        userImage.getLocationInWindow(userLocation);
+        enemyImage.getLocationInWindow(enemyLocation);
+
+        float startX = enemyLocation[0];
+        float startY = enemyLocation[1];
+        float endX = userLocation[0];
+        float endY = userLocation[1];
+
+        Animation enemyAttackAnimation = new TranslateAnimation(0, endX - startX, 0, endY - startY);
+        enemyAttackAnimation.setDuration(300);
+        enemyAttackAnimation.setFillAfter(true);
+        enemyAttackAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                if (enemyLutemonIsAlive) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Random random = new Random();
+                            int attackId = random.nextInt(4);
+                            enemyLutemon.attack(userLutemon, attackId);
+                            userLutemonHp.setText(userLutemon.getHealth() + "/" + userLutemon.getMaxHealth());
+                        }
+                    }, 600);
+                }
+
+                if (userLutemon.getHealth() <= 0) {
+                    userLutemonIsAlive = false;
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            destroyUserLutemon();
+                        }
+                    }, 200);
+
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation moveEnemyBackAnimation = new TranslateAnimation(endX - startX, 0, endY - startY, 0);
+                moveEnemyBackAnimation.setDuration(300);
+                moveEnemyBackAnimation.setFillAfter(true);
+                enemyImage.startAnimation(moveEnemyBackAnimation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        enemyImage.startAnimation(enemyAttackAnimation);
+
+
+    }
+
+    public void destroyEnemyLutemon() {
+        enemyImage.setImageResource(0);
+        enemyLutemonName.setText("");
+        enemyLutemonHp.setText("");
+    }
+
+    public void destroyUserLutemon() {
+
+        userImage.setImageResource(0);
+        userLutemonName.setText("");
+        userLutemonHp.setText("");
     }
 }
